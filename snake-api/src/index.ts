e95b05bc3eb081655c1e13746a508173c93d9fbf
Nick Mozghovyi <mykola.mozghovyi@bolt.eu>
2021-09-06 15:51:09 +0300

    initial swagger config (swagger-ui-express + swagger-jsdoc)

--- snake-api/src/index.ts
+++ snake-api/src/index.ts
@@ -1,11 +1,33 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+
 const app = express();
 const port = 8080; // default port to listen
 
-// define a route handler for the default home page
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 app.get("/", (req, res) => {
     res.send("Hello world!");
 });
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerJsdoc({
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Snake API",
+            version: '1.0.0',
+        },
+    },
+    apis: ['./dist/*.js'], // files containing annotations as above
+})));
 
 // start the Express server
 app.listen(port, () => {
