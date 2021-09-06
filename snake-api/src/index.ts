20ca08bbad9d13ed7a69a393620d7dd0769d772b
Nick Mozghovyi <mykola.mozghovyi@bolt.eu>
2021-09-06 16:50:56 +0300

    swagger moved to its route

--- snake-api/src/index.ts
+++ snake-api/src/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
-import swaggerUi from "swagger-ui-express";
-import swaggerJsdoc from "swagger-jsdoc";
+import swagger from './routes/swagger'
 
 const app = express();
 const port = 8080; // default port to listen
@@ -18,16 +17,7 @@ app.get("/", (req, res) => {
     res.send("Hello world!");
 });
 
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerJsdoc({
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "Snake API",
-            version: '1.0.0',
-        },
-    },
-    apis: ['./dist/*.js'], // files containing annotations as above
-})));
+app.use('/swagger',  swagger);
 
 // start the Express server
 app.listen(port, () => {
